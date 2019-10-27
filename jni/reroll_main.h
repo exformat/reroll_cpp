2d186075be56122c6cc278339212799e96220e84
exformat <exformat1988@gmail.com>
2019-10-27 23:18:28 +0600

    in trash

--- jni/reroll_main.h
+++ jni/reroll_main.h
@@ -1,5 +1,19 @@
+#ifndef
 
+#include "drive/DriveControl.h"
+#include "relay/Relay.h"
+#include "GyverLibs/Encoder.h"
+#include "GyverLibs/GyverButton.h"
 
-class main{
+class Reloll_main{
+	private:
+		Relay _drv_rly;
+		DriveControl _drv;
+		
+	public:
+		Reroll_main();
+		void update();
 	
-}
+};
+
+#endif
