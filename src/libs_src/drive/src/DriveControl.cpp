08291b7e011547ffb0846dd155ff5565ec0089ea
exformat <exformat1988@gmail.com>
2020-01-06 22:08:33 +0600

    add smooth functional

--- src/libs_src/drive/src/DriveControl.cpp
+++ src/libs_src/drive/src/DriveControl.cpp
@@ -1,44 +1,100 @@
 #include "DriveControl.h"
 #include "Arduino.h"
+#include <cmath>
+
+using namespace std;
 
 DriveControl::DriveControl(){}
-DriveControl::DriveControl(int pin, int start_drive_value){
+DriveControl::DriveControl(int pin, int start_pwm){
 	_pin = pin;
-	_srt_drv_val = start_drive_value;
+	_start_pwm = start_pwm;
+	old_tick_time = 0;
+	tick_time = 0;
+	delta_time = 0;
 	pinMode(_pin, OUTPUT);
 }
 
 void DriveControl::tick(){
+	old_tick_time = tick_time;
 	tick_time = millis();
+	delta_time = (tick_time - old_tick_time) / 1000;
+	
+	if(smooth){
+		smoothFunction();
+	}
 }
 	
 void DriveControl::stop(){
 	analogWrite(_pin, 0);
-	_rpm = 0;
+	_pwm = 0;
 }
 
 void DriveControl::start(){
-	_rpm = _srt_drv_val;
-	analogWrite(_pin, _rpm);
+	_pwm = _srt_drv_val;
+	analogWrite(_pin, _pwm);
 }
 
-void DriveControl::accel(int accel){
-	if(_rpm < MAX_RPM && _rpm > 0){
-		_rpm += accel;
-		analogWrite(_pin, _rpm);
+void DriveControl::accel(int pwm){
+	if(_pwm < MAX_PWM && _pwm > 0){
+		_pwm += pwm;
+		analogWrite(_pin, _pwm);
 	}
 }
 
-void DriveControl::smooth_start(){}
-void DriveControl::smooth_start(int duration){}
-void DriveControl::smooth_stop(){}
-void DriveControl::smooth_stop(int duration){}
-void DriveControl::smooth_accel(int target_rpm, int duration){}
+void DriveControl::smoothFunction(){
+	smooth_timer -= delta_time;
+	target_smoot_timer += delta_time;
+	if(target_pwm > 0){
+		smoorh_pwm_step = 1;
+	}
+	else{
+		smoorh_pwm_step = -1;
+	}
+	if(smooth_timer > 0){
+		if(target_smooth_timer >= smooth_step){
+			accel(smoorh_pwm_step);
+			target_smooth_timer = 0;
+		}
+	}
+}
+
+
+void DriveControl::smooth_start(){
+	target_pwm = _start_pwm;
+	smooth_timer = 2;
+	smooth_step = target_pwm / (smooth_timer * 1000);
+	smooth = true;
+}
+
+void DriveControl::smooth_start(int duration){
+	target_pwm = _srt_drv_val;
+	smooth_timer = duration;
+	smooth_step = target_pwm / (smooth_timer * 1000);
+	smooth = true;
+}
+void DriveControl::smooth_stop(){
+	target_pwm = 0;
+	smooth_timer = 2;
+	smooth_step = target_pwm / (smooth_timer * 1000);
+	smooth = true;
+}
+void DriveControl::smooth_stop(int duration){
+	target_pwm = 0;
+	smoorh_timer = duration;
+	smooth_step = target_pwm / (smooth_timer * 1000);
+	smooth = true;
+}
+void DriveControl::smooth_accel(int pwm, int duration){
+	target_pwm = pwm;
+	smootj_timer = duration;
+	smooth_step = target_pwm / (smooth_timer * 1000);
+	smooth = true;
+}
 
 void DriveControl::set_pin(int pin){
 	_pin = pin;
 }
-void DriveControl::set_start_drive_value(int val){
-	_srt_drv_val = val;
+void DriveControl::set_start_pwm(int pwm){
+	_start_pwm = pwm;
 }
 
--- src/libs_src/drive/src/DriveControl.h
+++ src/libs_src/drive/src/DriveControl.h
@@ -2,28 +2,39 @@
 #define DriveControl_h
 #include "Arduino.h"
 
-#define MAX_RPM 255
+#define MAX_PWM 254
 
 class DriveControl{
 	private:
 		int _pin;
-		int _rpm;
-		int _srt_drv_val;
+		int _pwm;
+		int target_pwm = 0;
+		int _start_pwm;
 		long tick_time;
+		long old_tick_time;
+		float delta_time;
+		
+		boolean smooth = false;
+		float target_smoot_timer = 0;
+		float smooth_timer = 0;
+		float smooth_step = 0;
+		int smoorh_pwm_step = 0;
+		
+		void smoothFunction();
 	
 	public:
 		DriveControl();
-		DriveControl(int pin, int start_drive_value);
+		DriveControl(int pin, int start_pwm);
 		void tick();
 		void stop();
 		void start();
-		void accel(int target_rpm);
+		void accel(int pwm);
 		void smooth_start();
 		void smooth_start(int duration);
-		void smooth_stop();
-		void smooth_stop(int duration);
-		void smooth_accel(int target_rpm, int duration);
+		void smoothStop();
+		void smoothStop(int duration);
+		void smooth_accel(int pwm, int duration);
 		void set_pin(int pin);
-		void set_start_drive_value(int val);
+		void set_start_pwm(int val);
 };
 #endif
