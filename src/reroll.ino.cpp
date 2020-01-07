a95847e3b3702936de7c5815b679598c1212edef
exformat <exformat1988@gmail.com>
2020-01-07 11:01:34 +0600

    Update reroll.ino.cpp

--- src/reroll.ino.cpp
+++ src/reroll.ino.cpp
@@ -3,21 +3,38 @@
 #include "RelayControl.h"
 #include "GyverButton.h"
 
-#define DRV_PIN   11
-#define ENC_L_PIN 4
-#define ENC_R_PIN 5
-#define RL_PIN    12
-#define BTN_STP   8
-#define BTN_SRT   7
+#include "Wire.h"
+#include "LiquidCrystal_I2C.h"
+#include "LiquidCrystal_PCF8574.h"
+
 #define HALL_SNR  2
 
-DriveControl drive(DRV_PIN, 30);
-Encoder encoder(ENC_L_PIN, ENC_R_PIN, 6, TYPE2);
-Relay relay(RL_PIN);
+#define ENC_CLK_PIN 4
+#define ENC_DT_PIN  5
+#define ENC_SW_PIN  6
+
+#define BTN_SRT   7
+#define BTN_STP   8
+
+#define DRV_PIN   9
+#define DRV2_PIN 10
+
+#define UP_SNR_PIN 11
+#define DN_SNR_PIN 12
+
+DriveControl roll_drive(DRV_PIN, 30);
+DriveControl punch_drive(DRV2_PIN, 30);
+
+Encoder encoder(ENC_CLK_PIN, ENC_DT_PIN, ENC_SW_PIN, TYPE1);
+//Relay relay(RL_PIN);
 GButton btn_stp(BTN_STP);
 GButton btn_srt(BTN_SRT);
+GButton up_sensor(UP_SNR_PIN);
+GButton dn_sensor(DN_SNR_PIN);
 
-boolean work = false;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+boolean work = true;
 volatile boolean sensor_trig = false;
 long strg_time = 0;
 long old_strg_time = 1;
@@ -27,121 +44,191 @@ long tick_time = 0;
 long old_tick_time = 1;
 long delta_time = 0;
 
-int target = 0;
+long lcd_time = 0;
+
+int target = 3000;
 float material_counter = 0;
 float step_counter = 0;
-const int STEP = 25;
+const float STEP = 27.75f;
 const int MAT_STEP = 200;
 
-float mpm = 0;
-int target_mpm = 0;
-const int MAX_MPM = 100;
+int rpm = 0;
 
-void setup(){
-	Serial.begin(9600);
-	Serial.println("hello, im iroller!");
-	attachInterrupt(digitalPinToInterrupt(HALL_SNR), sensor_control, RISING);
-	tick_time = millis();
+volatile float mpm = 0;
+
+String trg_str = "";
+String mpm_str = "";
+
+void setup() {
+
+  Serial.begin(9600);
+  Serial.println("hello, im iroller!");
+  attachInterrupt(digitalPinToInterrupt(HALL_SNR), sensor_control, RISING);
+  tick_time = millis();
+
+  lcd.init();
+  lcd.backlight();
+  //lcd.setCursor(0, 0);
+  //lcd.print("hello world");
+
 }
 
-void loop(){
-	time_control();
+void loop() {
+  time_control();
 
-	tick();
-	mpm_control();
-	material_control();
-	encoder_control();
-	button_control();
+  tick();
+  mpm_control();
+  print();
+
+
+
+  material_control();
+  encoder_control();
+  //button_control();
 }
 
-void mpm_control(){
-	//calculation of the time between sensor triggering
-	if(sensor_trig){
-        material_counter += STEP;
-        step_counter++;
+void print() {
 
-        old_strg_time = strg_time;
-        strg_time = tick_time;
-        delta_strg = strg_time - old_strg_time;
-        sensor_trig = false;
+  lcd.setCursor(0, 0);
+  lcd.print(mpm_str);
+  lcd.setCursor(5, 0);
+  lcd.print(rpm);
+  lcd.setCursor(0, 1);
+  lcd.print(trg_str);
+  lcd.setCursor(5, 1);
+  lcd.print(target);
+
+  if (lcd_time > 1000) {
+    // Serial.println(mpm);
+    lcd_time = 0;
+
+
+
+  }
+  lcd_time += delta_time;
+}
+
+
+
+boolean brake = false;
+void material_control() {
+  if(target - material_counter <= 500 && !brake){
+    roll_drive.smoothStop();
+  }
+  
+  if (material_counter >= target) {
+    //relay.off();
+    roll_drive.stop();
+    punch_drive.stop();
+    work = false;
+  }
+}
+
+
+void button_control() {
+  if (btn_srt.isPress()) {
+    if (work) {
+      roll_drive.stop();
+      punch_drive.stop();
+      work = false;
     }
-	//calculation of meters per minute
-    float f = delta_strg;
-    mpm = STEP / (f / 1000 / 60) / 1000;
-	
-	if(mpm < target_mpm){
-		drive.accel(1);
-	}
-	else{
-		drive.accel(-1);
-	}
-}
-
-void time_control(){
-    tick_time = millis();
-    
-	if(tick_time > old_tick_time){
-		delta_time = tick_time - old_tick_time;
-		old_tick_time = tick_time;
-	}
-}
-
-void material_control(){
-	if(material_counter >= target){
-		relay.off();
-		drive.stop();
-		work = false;
-	}
-}
-
-void sensor_control(){
-	if(!sensor_trig){
-		sensor_trig = true;
+    else {
+      roll_drive.start();
+      punch_drive.start();
+      work = true;
     }
+  }
 }
 
-void button_control(){
-	if(btn_srt.isPress()){
-        if(work){
-			relay.off();
-			drive.stop();
-			work = false;
-        }
-        else{
-			relay.on();
-			drive.start();
-			work = true;
-        }
-	}
+
+boolean target_setup = false;
+void encoder_control() {
+  if (!target_setup || work) {
+    if (encoder.isRight() && rpm < 250) { //acceleration increment
+      lcd.clear();
+      rpm++;
+      punch_drive.accel(1);
+      Serial.println("accel incr.");
+    }
+    if (encoder.isLeft() && rpm > 0) { //acceleration decrement
+      lcd.clear();
+      rpm--;
+      punch_drive.accel(-1);
+      roll_drive.accel(-1);
+      Serial.println("accel decr.");
+    }
+  }
+  else {
+    if (encoder.isRight()) {
+      Serial.println("target incr.");
+      lcd.clear();
+
+      target += MAT_STEP;
+    }
+    if (encoder.isLeft() && target > MAT_STEP) {
+      lcd.clear();
+      target -= MAT_STEP;
+      Serial.println("target decr.");
+    }
+  }
+  if (encoder.isClick()) {
+    target_setup = !target_setup;
+    if (target_setup) {
+      trg_str = "* trg: ";
+      mpm_str = "  mpm: ";
+    }
+    else {
+      trg_str = "  trg: ";
+      mpm_str = "* mpm: ";
+    }
+  }
 }
 
-void encoder_control(){
-	if(work){
-		if(encoder.isRight()){
-			Serial.println("accel incr.");
-			target_mpm = target_mpm < MAX_MPM ? target_mpm++ : MAX_MPM;
-		}
-		if(encoder.isLeft()){
-			Serial.println("accel decr.");
-			target_mpm = target_mpm > 0 ? target_mpm-- : 0;
-		}
-	}
-	else{
-		if(encoder.isLeft()){
-			Serial.println("target incr.");
-			target += MAT_STEP;
-		}
-		if(encoder.isRight() && target > MAT_STEP){
-			Serial.println("target decr.");
-			target -= MAT_STEP;
-		}
-	}
+void drivesControl() {
+  if (up_sensor.isPress()) {
+    roll_drive.accel(-5);
+  }
+  if (dn_sensor.isPress()) {
+    roll_drive.accel(5);
+  }
 }
 
-void tick(){
-	drive.tick();
-	encoder.tick();
-	btn_stp.tick();
-	btn_srt.tick();
+void sensor_control() {
+  if (!sensor_trig) {
+    sensor_trig = true;
+  }
 }
 
+void time_control() {
+  tick_time = millis();
+
+  if (tick_time > old_tick_time) {
+    delta_time = tick_time - old_tick_time;
+    old_tick_time = tick_time;
+  }
+}
+
+void mpm_control() {
+  //calculation of the time between sensor triggering
+  if (sensor_trig) {
+    material_counter += STEP;
+    step_counter++;
+    strg_time = tick_time;
+    old_strg_time = tick_time;
+
+    sensor_trig = false;
+  }
+  //calculation of meters per minute
+  delta_strg = tick_time - old_strg_time;
+  float f = delta_strg;
+  mpm = STEP / (f / 1000 / 60) / 1000;
+}
+
+void tick() {
+  roll_drive.tick();
+  punch_drive.tick();
+
+  encoder.tick();
+  btn_stp.tick();
+  btn_srt.tick();
+}
